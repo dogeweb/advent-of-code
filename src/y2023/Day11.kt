da727c46f6bd327632c5635166d3dfbab9a4d1ed
dogeweb <simonex720@gmail.com>
2025-01-09 19:14:53 +0100

    09-01-2025

--- src/y2023/Day11.kt
+++ src/y2023/Day11.kt
@@ -7,36 +7,7 @@ import kotlin.time.measureTimedValue
 
 fun main() {
 
-    fun part1(input: List<String>): Int {
-
-        val vertExp = input.flatMap {
-            if ('#' !in it) { listOf(it, it) } else listOf(it)
-        }
-
-        val horzExp = (0..vertExp[0].lastIndex)
-            .map { index -> vertExp.map { it.elementAt(index) }.joinToString("") }
-            .flatMap { if ('#' !in it) { listOf(it, it) } else listOf(it) }
-
-        val final = (0..horzExp.lastIndex)
-            .map { horzExp.filterIndexed { i, _ -> i == it }.joinToString("") }
-
-        val list = buildList {
-            final.forEachIndexed { y, s->
-                s.forEachIndexed { x, st ->
-                    if(st == '#') add(x to y)
-                }
-            }
-        }
-
-        val points = mutableSetOf(*list.toTypedArray())
-
-        return list.sumOf {
-            points.remove(it)
-            points.sumOf { o -> (it.first - o.first).absoluteValue + (it.second - o.second).absoluteValue }
-        }
-    }
-
-    fun part2(input: List<String>, mult: Long): Long {
+    fun solve(input: List<String>, mult: Long): Long {
 
         val vIndex = input.mapIndexedNotNull { index, it ->
             if ('#' !in it) index else null
@@ -72,13 +43,11 @@ fun main() {
     }
 
     val testInput = readInput("Day11_test")
-    check(part1(testInput).apply { println() } == 374)
-    check(part2(testInput, 2L).apply { println() } == 374L)
-    check(part2(testInput, 10L).apply { println() } == 1030L)
-    check(part2(testInput, 100L).apply { println() } == 8410L)
+    check(solve(testInput, 2L).apply { println() } == 374L)
+    check(solve(testInput, 10L).apply { println() } == 1030L)
+    check(solve(testInput, 100L).apply { println() } == 8410L)
 
     val input = readInput("Day11")
-    measureTimedValue { part1(input) }.println()
-    measureTimedValue { part2(input, 2L) }.println()
-    measureTimedValue { part2(input, 1000000) }.println()
+    measureTimedValue { solve(input, 2L) }.println()
+    measureTimedValue { solve(input, 1000000) }.println()
 }
---
+++ src/y2023/Day12.kt
@@ -0,0 +1,39 @@
+package y2023
+
+import kotlin.time.measureTimedValue
+
+fun main() {
+
+    fun solve(input: List<String>, part2: Boolean = false): Long {
+
+        val map = mutableMapOf<Pair<String, List<Int>>, Long>()
+        val qmRegex = "^\\.*\\?".toRegex()
+        fun regex(int: Int) = "^\\.*[#?]{${int}}(?:[.?]|\$)".toRegex()
+
+        val rec = DeepRecursiveFunction<Pair<String, List<Int>>, Long> { (s, nums) ->
+            map.getOrPut(s to nums) {
+                if (nums.isEmpty()) if ('#' in s) 0 else 1
+                else (regex(nums[0]).find(s)?.let { callRecursive(s.drop(it.range.last + 1) to nums.drop(1)) }
+                    ?: 0) + (qmRegex.find(s)?.let { callRecursive(s.drop(it.range.last + 1) to nums) } ?: 0)
+            }
+        }
+
+        return if (!part2)
+            input.sumOf { it.split(" ").let { (a, b) -> rec(a to b.split(",").map(String::toInt)) } }
+        else
+            input.sumOf {
+                it.split(" ").let { (a, b) ->
+                    rec(List(5) { a }.joinToString("?") to
+                            b.split(",").map(String::toInt).let { r -> List(5) { r }.flatten() })
+                }
+            }
+    }
+
+    val testInput = readInput("Day12_test")
+    check(solve(testInput).apply { println() } == 21L)
+    check(solve(testInput, true).apply { println() } == 525152L)
+
+    val input = readInput("Day12")
+    measureTimedValue { solve(input) }.println()
+    measureTimedValue { solve(input, true) }.println()
+}
